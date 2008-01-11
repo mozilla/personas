f22b343517b15d2d4f95fc24d1be5a91ef080c2b
Myk Melez <myk@mozilla.org>
2008-01-10 19:14:36 -0500

    stop packaging files that shouldn't be part of the XPI, and stop horking chrome.manifest when building a XPI

--- build.sh
+++ build.sh
@@ -7,6 +7,9 @@ jar="# "
 if [[ $1 == xpi ]]; then
     unpacked="# "
     jar=
+    # Back up the chrome.manifest file so we can restore it to its pristine
+    # state after horking it to point to the JAR archives in the XPI file.
+    cp chrome.manifest .chrome.manifest.bak
 fi
 
 for in in `find . -type f -name \*.in`; do
@@ -25,8 +28,17 @@ cd -
 if [[ ! $1 == xpi ]]; then
     exit 0;
 fi
-echo "foo"
+
 cd chrome
-zip -9 -ur personas.jar *
+# \*/CVS/\* excludes all files in CVS directories
+# \*/.\* excludes all hidden Unix files (i.e. those that start with a period)
+zip -9 -ur personas.jar content locale skin -x \*/CVS/\* -x \*/.\*
 cd ..
-zip -9 -ur personas.xpi chrome/personas.jar defaults components install.rdf chrome.manifest
+
+# components/\*.idl excludes IDL files, which are unnecessary in the XPI
+#   since the info in them has been compiled to an XPT file
+# components/xptgen excludes that script, which is part of the build process
+zip -9 -ur personas.xpi chrome/personas.jar components defaults install.rdf chrome.manifest -x \*/CVS/\* -x \*/.\* -x components/\*.idl -x components/xptgen
+
+# Restore the backed up chrome.manifest file.
+cp .chrome.manifest.bak chrome.manifest
