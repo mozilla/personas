1d81d4baa6cbb0b13a389161cdc63f17057a1289
Myk Melez <myk@mozilla.org>
2009-03-06 13:29:56 -0800

    import new version of Observers module (only comment changes)

--- client/modules/Observers.js
+++ client/modules/Observers.js
@@ -51,6 +51,21 @@ Cu.import("resource://gre/modules/XPCOMUtils.jsm");
  * @version 0.2
  */
 let Observers = {
+  /**
+   * Register the given callback as an observer of the given topic.
+   *
+   * @param   topic       {String}
+   *          the topic to observe
+   *
+   * @param   callback    {Object}
+   *          the callback; an Object that implements nsIObserver or a Function
+   *          that gets called when the notification occurs
+   *
+   * @param   thisObject  {Object}  [optional]
+   *          the object to use as |this| when calling a Function callback
+   *
+   * @returns the observer
+   */
   add: function(topic, callback, thisObject) {
     let observer = new Observer(topic, callback, thisObject);
     this._cache.push(observer);
@@ -59,6 +74,18 @@ let Observers = {
     return observer;
   },
 
+  /**
+   * Unregister the given callback as an observer of the given topic.
+   *
+   * @param   topic       {String}
+   *          the topic being observed
+   *
+   * @param   callback    {Object}
+   *          the callback doing the observing
+   *
+   * @param   thisObject  {Object}  [optional]
+   *          the object being used as |this| when calling a Function callback
+   */
   remove: function(topic, callback, thisObject) {
     // This seems fairly inefficient, but I'm not sure how much better
     // we can make it.  We could index by topic, but we can't index by callback
@@ -79,14 +106,15 @@ let Observers = {
    * @param topic   {String}
    *        the topic to notify observers about
    *
-   * @param subject {Object}
+   * @param subject {Object}  [optional]
    *        some information about the topic; can be any JS object or primitive
    *
-   * @param data    {String}  [deprecated]
-   *        some more information about the topic; deprecated, as the subject
-   *        is sufficient to pass all needed information; if you have multiple
-   *        pieces of information, pass it inside an anonymous object, i.e.:
-   *        { foo: 1, bar: "some string", baz: myObject }
+   * @param data    {String}  [optional] [deprecated]
+   *        some more information about the topic; deprecated as the subject
+   *        is sufficient to pass all needed information to the JS observers
+   *        that this module targets; if you have multiple values to pass to
+   *        the observer, wrap them in an object and pass them via the subject
+   *        parameter (i.e.: { foo: 1, bar: "some string", baz: myObject })
    */
   notify: function(topic, subject, data) {
     subject = (typeof subject == "undefined") ? null : new Subject(subject);
