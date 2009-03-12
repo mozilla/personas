20579598d53511cabd81c638fe034e1a30ba6a71
Toby Elliott <telliott@mozilla.com>
2009-03-12 11:36:39 -0700

    rolling some shared functions centrally

--- server/viewpersonas.php
+++ server/viewpersonas.php
@@ -1,26 +1,8 @@
 <?php 
 	require_once 'lib/personas_constants.php';	
+	require_once 'lib/personas_functions.php';	
 	require_once 'lib/storage.php';
 	
-	function url_prefix($id)
-	{
-		$second_folder = $id%10;
-		$first_folder = ($id%100 - $second_folder)/10;
-		return  $first_folder . '/' . $second_folder .  '/'. $id ;
-	}
-
-	function extract_record_data($item)
-	{
-		$padded_id = $item{'id'} < 10 ? '0' . $item{'id'} : $item{'id'};
-		$extracted = array('id' => $item{'id'}, 
-						'name' => $item{'name'},
-						'accentcolor' => $item{'accentcolor'} ? '#' . $item{'accentcolor'} : null,
-						'textcolor' => $item{'textcolor'} ? '#' . $item{'textcolor'} : null,
-						'header' => url_prefix($item{'id'}) . '/' . $item{'header'}, 
-						'footer' => url_prefix($item{'id'}) . '/' . $item{'footer'});
-		return $extracted;	
-	}
-
 	$page_size = 21;
 
 	$db = new PersonaStorage();
