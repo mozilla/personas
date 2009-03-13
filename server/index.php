9a8b5a080a3c01c21eda6bea1776593f36153d37
Ryan Doherty <rdoherty@mozilla.com>
2009-03-13 13:21:17 -0500

    links

--- server/index.php
+++ server/index.php
@@ -4,19 +4,19 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<title>Personas Homepage</title>
-	<link href="/store/css/style.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
 </head>
 <body class="home">
     <div id="outer-wrapper">
         <div id="inner-wrapper">
-            <p id="account"><a href="#">Designer Tools</a></p>
+            <p id="account"><a href="">Designer Tools</a></p>
             <div id="nav">
-                <h1><a href="#"><img src="/store/img/logo.png" alt="Mozilla Labs Personas"></a></h1>
+                <h1><a href="/store"><img src="/store/img/logo.png" alt="Mozilla Labs Personas"></a></h1>
                 <ul>
-                    <li class="gallery"><a href="#">Gallery</a></li>
+                    <li class="gallery"><a href="/store/dynamic/gallery/All/Popular">Gallery</a></li>
                     <li class="create"><a href="#">Create <br/>Your Own</a></li>
-                    <li class="demo"><a href="#">Demo</a></li>
-                    <li class="faq"><a href="#">Frequent <br/>Questions</a></li>
+                    <li class="demo"><a href="/store/demo_install.html">Demo</a></li>
+                    <li class="faq"><a href="/store/faq.html">Frequent <br/>Questions</a></li>
                 </ul>
             </div>
             <div id="header">
