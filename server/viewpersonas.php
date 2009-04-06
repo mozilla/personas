87a2d8e7421f438437a327cfb6908ce7399293c0
Toby Elliott <telliott@mozilla.com>
2009-04-06 09:19:52 -0700

    removing store link from breadcrumb

--- server/viewpersonas.php
+++ server/viewpersonas.php
@@ -40,7 +40,7 @@
                 Firefox.</h3>
             </div>
             <div id="maincontent">
-                <p id="breadcrumbs"><a href="http://www.getpersonas.com">Personas Home</a> : <a href="http://www.getpersonas.com/store/gallery/All/Popular">Gallery</a> : <?= $category ?><?php if ($tab != "All") { echo " : $tab"; } ?></p>
+                <p id="breadcrumbs"><a href="http://www.getpersonas.com">Personas Home</a> : <a href="http://www.getpersonas.com/gallery/All/Popular">Gallery</a> : <?= $category ?><?php if ($tab != "All") { echo " : $tab"; } ?></p>
                 <div id="gallery">
                     <ul>
 <?php
