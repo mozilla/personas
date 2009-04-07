361610382b19553341615f20b58c20785c9bcb00
Toby Elliott <telliott@mozilla.com>
2009-04-06 17:19:02 -0700

    checking in the dash

---
+++ server/admin/dashboard.php
@@ -0,0 +1,97 @@
+<?php
+
+# ***** BEGIN LICENSE BLOCK *****
+# Version: MPL 1.1/GPL 2.0/LGPL 2.1
+#
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+# The Original Code is Personas Server
+#
+# The Initial Developer of the Original Code is
+# Mozilla Labs.
+# Portions created by the Initial Developer are Copyright (C) 2008
+# the Initial Developer. All Rights Reserved.
+#
+# Contributor(s):
+#	Toby Elliott (telliott@mozilla.com)
+#
+# Alternatively, the contents of this file may be used under the terms of
+# either the GNU General Public License Version 2 or later (the "GPL"), or
+# the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+# in which case the provisions of the GPL or the LGPL are applicable instead
+# of those above. If you wish to allow use of your version of this file only
+# under the terms of either the GPL or the LGPL, and not to allow others to
+# use your version of this file under the terms of the MPL, indicate your
+# decision by deleting the provisions above and replace them with the notice
+# and other provisions required by the GPL or the LGPL. If you do not delete
+# the provisions above, a recipient may use your version of this file under
+# the terms of any one of the MPL, the GPL or the LGPL.
+#
+# ***** END LICENSE BLOCK *****
+	
+	require_once '../lib/personas_constants.php';
+	require_once '../lib/storage.php';
+	require_once '../lib/user.php';
+
+	
+	
+	$result = "";
+	$title = "Admin | Dashboard";
+
+	try 
+	{
+		$user = new PersonaUser();
+		$user->authenticate();
+		$user->force_signin(1);
+		if (!$user->has_admin_privs())
+		{
+			$_errors['error'] = 'This account does not have privileges for this operation. Please <a href="/signin?action=logout">log in</a> with an account that does.';
+			include '../templates/user_error.php';
+			exit;
+		}
+
+		$db = new PersonaStorage();
+
+		$pending = count($db->get_pending_personas());
+		$edits = count($db->get_pending_edits());
+		$total = $db->get_active_persona_count();
+	}
+	catch(Exception $e)
+	{
+		error_log($e->getMessage());
+		print("Database problem. Please try again later.");
+		exit;
+	}
+
+	include '../templates/header.php';
+?>
+<body>
+    <div id="outer-wrapper">
+        <div id="inner-wrapper">
+<?php include '../templates/nav.php'; ?>
+            <div id="maincontent">
+
+Personas Status:
+<p>
+Total Active Personas: <?= $total ?>
+<p>
+New Pending Personas: <a href="pending.php" target="_blank"><?= $pending ?></a>
+<p>
+Personas Awaiting Edit Approval: <a href="editing.php" target="_blank"><?= $edits ?></a>
+<p><hr><p>
+<a href="/store/dynamic/gallery/All/" target="_blank">Edit/Pull Personas</a>
+<p>
+<a href="https://labs.mozilla.com/forum/?CategoryID=18" target="_blank">Personas Discussion Board</a>
+
+
+<?php include '../templates/footer.php'; ?>
+</body>
+</html>
