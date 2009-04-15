19cda8a27a05ba161160292dc30a112a55c402a0
Myk Melez <myk@mozilla.org>
2009-04-15 10:33:45 -0700

    bug 488445: trust personas.services.mozilla.com to preview, reset, and set personas

--- client/defaults/preferences/personas.js
+++ client/defaults/preferences/personas.js
@@ -34,7 +34,7 @@ pref("extensions.personas.siteURL", "http://www.getpersonas.com/store/");
 // of domains allowed to set and preview personas.
 // At a minimum, it must contain a value matching the domain at which the web
 // directory is located in order for the directory to work.
-pref("extensions.personas.authorizedHosts", "getpersonas.com, www.getpersonas.com");
+pref("extensions.personas.authorizedHosts", "getpersonas.com, www.getpersonas.com, personas.services.mozilla.com");
 
 pref("extensions.personas.lastselected0", "");
 pref("extensions.personas.lastselected1", "");
