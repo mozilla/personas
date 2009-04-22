d7b6cfd244af33d7506c4bb65ecbbedc931e2cf7
Toby Elliott <telliott@mozilla.com>
2009-04-22 14:52:57 -0700

    using the right variable name

--- server/templates/featured_designer.php
+++ server/templates/featured_designer.php
@@ -26,7 +26,7 @@
 ?>
                         <li>
                             <img class="preview persona" src="<?= PERSONAS_LIVE_PREFIX . '/' . url_prefix($persona['id']) ?>/preview_featured.jpg" persona="<?= $persona_json ?>">
-                            <h4><a href="/gallery/Designer/<?= $item['author'] ?>"><?= $persona['author'] ?></a></h4>
+                            <h4><a href="/gallery/Designer/<?= $persona['author'] ?>"><?= $persona['author'] ?></a></h4>
                             <p class="try"><a href="<?= $detail_url ?>">view details »</a></p>
                             <hr />
                         </li>
