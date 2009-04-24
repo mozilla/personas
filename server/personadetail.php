c9804b233ed04de084eb7acecda0138fb8364b34
Ryan Doherty <rdoherty@mozilla.com>
2009-04-23 17:13:23 -0700

    bug 488494, sharethis widget

--- server/personadetail.php
+++ server/personadetail.php
@@ -65,10 +65,13 @@
                 <p id="buttons">
                     <a href="#" class="button" id="try-button" persona="<?= $persona_json ?>"><span>try it now</span><span>&nbsp;</span></a>
                 </p>
+                
 <?php
 		if ($persona_data['popularity'])
 			print '<p class="numb-users">' . number_format($persona_data['popularity']) . ' active daily users</p>';
-	
+	?>
+	<p><script type="text/javascript" src="http://w.sharethis.com/button/sharethis.js#publisher=df86b16e-195c-4917-ae28-61a1382ba281&amp;type=website&amp;send_services=&amp;post_services=facebook%2Cdigg%2Cdelicious%2Cybuzz%2Ctwitter%2Cstumbleupon%2Creddit%2Ctechnorati%2Cmixx%2Cblogger%2Ctypepad%2Cwordpress%2Cgoogle_bmarks%2Cwindows_live%2Cmyspace%2Cfark%2Cbus_exchange%2Cpropeller%2Cnewsvine%2Clinkedin"></script></p>
+	<?php
 	} else {
 ?>            
                 <p class="description">We are unable to find this persona. Please return to the gallery and try again.</p>
