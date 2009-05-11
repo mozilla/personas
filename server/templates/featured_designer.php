60a7540ceb22543c51eb8f5bba19e3ff1e86f233
Ryan Doherty <rdoherty@mozilla.com>
2009-05-11 15:45:50 -0700

    Bug 488490 -  Website: on homepage, add widget for featured designers

--- server/templates/featured_designer.php
+++ server/templates/featured_designer.php
@@ -33,6 +33,7 @@
                             <h4><a href="/gallery/Designer/<?= $persona['author'] ?>"><?= $persona['author'] ?></a></h4>
                             <p class="try"><a href="/gallery/Designer/<?= $persona['author'] ?>">view designer »</a></p>
                             <hr />
+                            <p><? //description goes here ?></p>
                         </li>
 <?php
 					}
