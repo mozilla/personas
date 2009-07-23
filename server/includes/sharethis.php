e39ceb612999786c61bd08dd61ed210142e973ff
Ryan Doherty <rdoherty@mozilla.com>
2009-07-23 09:31:55 -0700

    Bug 504477 -  replace "share this" code on designer and design detail pages

---
+++ server/includes/sharethis.php
@@ -0,0 +1,2 @@
+<script type="text/javascript"
+src="http://w.sharethis.com/button/sharethis.js#publisher=df86b16e-195c-4917-ae28-61a1382ba281&amp;type=website&amp;buttonText=Share%20With%20Friends!&amp;post_services=facebook%2Cmyspace%2Cdigg%2Ctwitter&amp;headerTitle="></script>
--- server/templates/gallery.php
+++ server/templates/gallery.php
@@ -87,7 +87,9 @@
  			}
  ?>
                     </ul>
-                    <?php if($category == 'Designer')  echo '<p><script type="text/javascript" src="http://w.sharethis.com/button/sharethis.js#publisher=df86b16e-195c-4917-ae28-61a1382ba281&amp;type=website&amp;send_services=&amp;post_services=facebook%2Cdigg%2Cdelicious%2Cybuzz%2Ctwitter%2Cstumbleupon%2Creddit%2Ctechnorati%2Cmixx%2Cblogger%2Ctypepad%2Cwordpress%2Cgoogle_bmarks%2Cwindows_live%2Cmyspace%2Cfark%2Cbus_exchange%2Cpropeller%2Cnewsvine%2Clinkedin"></script></p>'; ?>                    
+                    <?php if($category == 'Designer') { ?>                    
+                        <p><?php include('includes/sharethis.php'); ?></p>
+                    <?php } ?>
                 </div>
 <?php include 'pagination.php'; ?>
             </div>
