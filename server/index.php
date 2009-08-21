fd4813b9d0b036034cc7d16bd7c79d76c4b6388d
Mark Hahnenberg <mhahnenberg@mozilla.com>
2009-08-21 10:33:15 -0700

    Recommitted index.php from a botched merge.

--- server/index.php
+++ server/index.php
@@ -3,11 +3,12 @@
 	require_once 'lib/personas_functions.php';	
 	require_once 'lib/storage.php';
 	require_once 'lib/user.php';
-
-	$db = new PersonaStorage();
+    require_once 'lib/language.php';
+    
+    $db = new PersonaStorage();
 	$user = new PersonaUser();
-
-	$title = "Dress up your web browser"; 
+	
+    $title = _("Dress up your web browser"); 
 	include 'templates/header.php'; 
 ?>
 <body class="home">
@@ -15,24 +16,24 @@
         <div id="inner-wrapper">
 <?php include 'templates/nav.php'; ?>
 			<div id="header">
-                <h2>What will your browser wear today?</h2>
-                <h3>Personas are lightweight, easy-to-install and easy-to-change "skins" for your Firefox web browser.</h3>
+                <h2><?= _("What will your browser wear today?");?></h2>
+                <h3><?= _("Personas are lightweight, easy-to-install and easy-to-change \"skins\" for your Firefox web browser.");?></h3>
                 <div class="get-personas">
                     <div>
                         <p>
-                            <a href="https://addons.mozilla.org/en-US/firefox/downloads/latest/10900" class="get-personas" id="download"><span>Get Personas for Firefox - Free</span><span class="arrow"></span></a>
+                            <?= _("<a href=\"https://addons.mozilla.org/firefox/downloads/latest/10900\" class=\"get-personas\" id=\"download\"><span>Get Personas for Firefox - Free</span>");?><span class="arrow"></span></a>
                             <script type="text/javascript" charset="utf-8">
-                                $("#download").personasDownload({"bundle":"bundle-url", "bundle-text":'<span>Get Firefox and Personas - Free</span><span class="arrow"></span>'});
+                                $("#download").personasDownload({"bundle":"bundle-url", "bundle-text":'<span><?= _("Get Firefox and Personas - Free");?></span><span class="arrow"></span>'});
                             </script>
                         </p>
-                        <p class="platforms-note">Firefox Add-on for Windows, Mac or Linux</p>
+                        <p class="platforms-note"><?= _("Firefox Add-on for Windows, Mac or Linux");?></p>
                     </div>
                 </div>
                 
                 <div id="more-info">
                     <div id="info">
-                        <h4>Theme your browser according to your mood, hobby or season. </h4>
-                        <p>Click the green download button to get started!</p>                  
+                        <h4><?= _("Theme your browser according to your mood, hobby or season.");?> </h4>
+                        <p><?= _("Click the green download button to get started!");?></p>                  
                     </div>
                 </div>
             </div>
@@ -48,8 +49,8 @@
             $(".slideshow").slider();
             $("#more-info").popup();
             
-            $("#header").ie6Warning({"message":'<div id="ie6">Upgrade your browser to get the most out of this website. <a href="%LINK%">Download Firefox for free</a>.</div>'});
-            $("img.persona").previewPersona(true);
+            $("#header").ie6Warning({"message":'<div id="ie6"><?= _("Upgrade your browser to get the most out of this website. <a href=\"%LINK%\">Download Firefox for free</a>.");?></div>'});
+            $("img.persona").previewPersona();
         });
     </script>
 </body>
