7897409038a6e68f88a4bd3ff78df22d439ec4ae
Myk Melez <myk@mozilla.org>
2009-10-12 18:01:45 -0700

    remove obsolete server files now that they're being tracked in Subversion and have been migrated over to it

--- server/templates/footer.php
+++
@@ -1,9 +0,0 @@
-    <div id="footer">
-        <p>Copyright © <?= date("Y") ?> Mozilla. <a href="http://labs.mozilla.com/projects/firefox-personas/">Personas</a> is a <a href="http://labs.mozilla.com">Mozilla Labs</a> experiment. | <a href="http://labs.mozilla.com/about-labs/">About Mozilla Labs</a>    |  <a href="/privacy">Privacy</a> | <a href="/dashboard">Dashboard</a></p>
-    </div>
-
-    
-	<script src="/static/js/urchin.js"></script>
-    <script type="text/javascript">
-          urchinTracker();
-    </script>
