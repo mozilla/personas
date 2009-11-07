21ae005fa340fff6d8be53203d584283eb61e2e0
Myk Melez <myk@mozilla.org>
2009-11-06 18:46:48 -0800

    update max version compatibility to the upcoming stable 3.6.* releases of Firefox and 3.0.* releases of Thunderbird

--- client/client.mk
+++ client/client.mk
@@ -60,7 +60,7 @@ site_path_remote  := people.mozilla.com:/home/cbeard/public_html/personas/dist
 # the extension is compatible.  These are used in both the install and update
 # manifests.
 fx_min_version    := 3.0
-fx_max_version    := 3.6b1pre
+fx_max_version    := 3.6.*
 
 tb_min_version    := 3.0b1
-tb_max_version    := 3.0pre
+tb_max_version    := 3.0.*
